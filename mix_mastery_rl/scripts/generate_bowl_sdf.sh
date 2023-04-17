68b9d06fa732f6999f49862f51dadd35b56dfadd
sattyorin <sattyorin.sattyon@gmail.com>
2023-04-17 21:45:38 +0900

    added script of bowl sdf generator

---
+++ mix_mastery_rl/scripts/generate_bowl_sdf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# bowl_sdf_generator
+
+cd $(dirname $0)/../..
+pwd
+
+path=mix_mastery_rl/envs/mixing/config/bowl_param.yaml
+ls -la $path
+
+radius_top=$(yq e '.radius_top' $path)
+radius_bottom=$(yq e '.radius_bottom' $path)
+height=$(yq e '.height' $path)
+num_geom=$(yq e '.num_geom' $path)
+
+python make_bowl.py --model_name cooking_bowl \
+                    --num_division $num_geom \
+                    --radius_bottom $radius_bottom \
+                    --radius_top $radius_top \
+                    --height $height \
+                    --thickness 0.006 \
+                    --out_mujoco_dir . \
+                    --out_gazebo_dir models
